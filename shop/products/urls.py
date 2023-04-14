ad041f2bf9c10d914f2bfa40975ac9fecfa5169b
Muhammaddiyor <muhammaddiyorashurov5@gmail.com>
2023-04-14 20:29:57 +0300

    add login and register page

--- shop/products/urls.py
+++ shop/products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('products/', views.products, name='products')
+    path('products/', views.products, name='products'),
+    path("products/<int:category_id>", views.products, name='category'),
+    path("products/page/<int:page>", views.products, name='page'),
 ]
--- shop/products/views.py
+++ shop/products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Product, ProductCategory
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -9,8 +11,18 @@ def index(request):
     return render(request, 'products/index.html', context=context)
 
 
-def products(request):
+def products(request, category_id=None):
     context = {
-        'title': "Market Place - Catalog"
+        'title': "Market Place - Catalog",
+        'categories': ProductCategory.objects.all(),
     }
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    paginator = Paginator(products, 3)  # saifada 3 dona tavarni ko'rsatadi
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context.update({"products": page_obj})
     return render(request, 'products/products.html', context=context)
